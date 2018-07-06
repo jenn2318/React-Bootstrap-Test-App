45600d36753da1bd4f51b6f4fc9b250df17c7acf
Jennifer Ponder <jennp@Jennifers-MacBook-Pro.local>
2018-07-06 14:50:06 -0400

    updated these files

--- Test/react-bootstrap/src/App.js
+++ Test/react-bootstrap/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './components/Home';
+import About from './componets/About';
+import LastCallEats from './components/LastCallEats';
 import './App.css';
+import Navbar from './components/CustomNavbar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+        <Router>
+        <div>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+          <Route path="/About" component={About} />
+          <Route path="/LastCallEats" component={LastCallEats} />
+        </div>
+       </Router>
+
     );
   }
 }
